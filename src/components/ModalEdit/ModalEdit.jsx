985e6141c5de50a4915a15959131f79065e86c9c
Oleh Purys <wenger1901@gmail.com>
2024-10-22 09:57:07 +0300

    update ModalEdit

--- src/components/ModalEdit/ModalEdit.jsx
+++ src/components/ModalEdit/ModalEdit.jsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsModalEditOpen } from '../../redux/modal/selectors';
 import { closeModalEdit } from '../../redux/modal/slice';
 import ModalEditForm from '../ModalEditForm/ModalEditForm';
 import css from './ModalEdit.module.css';
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
 
 export default function ModalEdit({ contact }) {
   const isModalEditOpen = useSelector(selectIsModalEditOpen);
@@ -25,13 +19,10 @@ export default function ModalEdit({ contact }) {
     <React.Fragment>
       <Dialog
         open={isModalEditOpen}
-        TransitionComponent={Transition}
-        keepMounted
         onClose={handleModalEditClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>Edit</DialogTitle>
-        <DialogContent>
+        <DialogContent sx={{ padding: 0 }}>
           <ModalEditForm contact={contact} onClose={handleModalEditClose} />
         </DialogContent>
       </Dialog>
